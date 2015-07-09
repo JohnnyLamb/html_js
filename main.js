4fa4c1fe6f387600dca5588d7f058e719c7df8c3
johnny lamb <agnusjack@gmail.com>
2015-07-09 13:57:21 -0600

    html and js file

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// function prompter() {
+// var reply = prompt("What's your name?", "")
+// alert ( "Hello " + reply + "!")
+// }
+
+var name = prompt("what's your name");
+alert("hello " + name);
+
+
+
+
+//
+// Use var, prompt, string literals, the + operator, and alert
+// such that when the user opens the page, it will ask them for their name,
+// and then respond with "Hello [NAME_GOES_HERE]!"
